b0bc9e0b343296efa937176df39a6fc8384ec9c2
Aman Nijjar <amannijjar19@gmail.com>
2023-08-10 23:00:29 -0700

    initial commit of new website: ready for deployment

--- resources/common/js/mainFunctions.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(() => {
-    console.log('ready');
-    $('.navbar_wraper').load('/resources/common/navbar.html');    
-    $('.footer_wraper').load('/resources/common/footer.html');
-});
-
